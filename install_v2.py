99c3db818705a313fe2e63b1d137bcc20eda829f
kainhuck <1628321264@qq.com>
2022-08-01 18:52:41 +0800

    add .

--- install_v2.py
+++ install_v2.py
@@ -4,8 +4,15 @@
 # The install script for Archlinux installation
 # =============================================
 
+from doctest import FAIL_FAST
 import os
+import re
 import sys
+import platform
+import subprocess
+
+
+# =================================== global values ====================================
 
 support_shells = ("bash", "zsh", "fish")
 support_desktops = ("gnome", "plasma")
@@ -13,256 +20,169 @@ support_language = ("en", "zh")
 
 base_packages = "base base-devel linux linux-firmware vim openssh zsh fish git wget curl grub dhcpcd net-tools"
 
+UEFI = "UEFI"
+BIOS = "BIOS"
 
-def print_red(a, end="\n"):
-    print(f"\033[31m{a}\033[0m", end=end)
+CPU_AMD = "AuthenticAMD"
+CPU_INTEL = "GenuineIntel"
 
-
-def print_green(a, end="\n"):
-    print(f"\033[32m{a}\033[0m", end=end)
-
-
-def print_yellow(a, end="\n"):
-    print(f"\033[33m{a}\033[0m", end=end)
+# ======================================================================================
 
 
-def print_blue(a, end="\n"):
-    print(f"\033[34m{a}\033[0m", end=end)
+# =================================== color function ===================================
+
+def apply_red(s:str) -> str:
+    return f"\033[31m{s}\033[0m"
+
+def apply_green(s:str) -> str:
+    return f"\033[32m{s}\033[0m"
 
 
-def print_purple(a, end="\n"):
-    print(f"\033[35m{a}\033[0m", end=end)
+def apply_yellow(s:str) -> str:
+    return f"\033[33m{s}\033[0m"
 
 
-def print_cyan(a, end="\n"):
-    print(f"\033[36m{a}\033[0m", end=end)
+def apply_blue(s:str) -> str:
+    return f"\033[34m{s}\033[0m"
 
 
-def just_run(prompt: str):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            print_blue(f"Now in {prompt}...")
-            func(*args, **kwargs)
-            print_yellow(f"{prompt} successfully finished")
-        return wrapper
-    return decorator
+def apply_purple(s:str) -> str:
+    return f"\033[35m{s}\033[0m"
 
 
-# Config:
-#   The configuration for installation, include hostname, disk partition ...
+def apply_cyan(s:str) -> str:
+    return f"\033[36m{s}\033[0m"
+
+# ======================================================================================
+
+
+def run_cmd(cmd: str, debug:bool=True, exit:bool=True) -> str:
+    if debug:
+        print("%s %s" % apply_cyan("[RUN]"), apply_yellow(cmd))
+
+    code, output = subprocess.getstatusoutput(cmd)
+
+    try:
+        assert code == 0
+    except AssertionError:
+        if exit:
+            print("%s" % apply_red(f"ERROR: {output}"))
+            sys.exit(code)
+        else:
+            return ""
+
+    return output
+
+
+class DiskMount:
+    def __init__(self, disk:str, mount_point:str):
+        self.disk = disk
+        self.mount_point = mount_point
+
+class User:
+    def __init__(self, name:str, passwd: str, shell:str="bash"):
+        self.name = name
+        self.passwd = passwd
+        self.shell = shell
+
+
 class Config:
     def __init__(self):
-        # boot_way: UEFI or BIOS
-        self.boot_way = None
-        # cpu_vendor: intel, amd
+        self.boot = None
         self.cpu_vendor = None
-        # disk_mount
-        # [
-        #   {
-        #     "disk": "/dev/sda",
-        #     "mount_point": "/"
-        #   },
-        #   {
-        #     "disk": "/dev/sdb",
-        #     "mount_point": "/home"
-        #   },
-        # ]
-        self.disk_mount = []
-        # desktop: None, Gnome, Plasma
+        self.install_disk = None
+        self.disk_mount = []   # DiskMount
         self.desktop = None
-        # root_passwd: passwd for root user
         self.root_passwd = None
-        # common_users:
-        #  [
-        #    {
-        #      "username": "kainhuck",
-        #      "password": "123456",
-        #      "shell": "base"
-        #    }
-        #  ]
-        self.common_users = []
-        # language: en, zh
+        self.common_users = [] # User
         self.language = None
-        # swap_size: size of swap
         self.swap_size = None
-        # hostname
         self.hostname = None
 
-        self._set_boot_way()
-        self._set_cpu_vendor()
+        # self._detect_platform()
+        self._detect_boot()
+        self._detect_cpu_vendor()
+    
+    def _detect_platform(self):
+        """自动检测平台，是否是archlinux安装环境"""
+        if platform.uname().system != "Linux" or platform.uname().node != "archiso":
+            print("%s" % apply_red("This script only for archlinux installation"))
+            sys.exit(0)
 
-    def _set_boot_way(self):
-        pass
-
-    def _set_cpu_vendor(self):
-        pass
-
-    @just_run("set_disk_mount")
-    def set_disk_mount(self):
-        pass
-
-    @just_run("set_desktop")
-    def set_desktop(self):
-        print_purple("Choose a desktop for your Archlinux")
-        print_green("0. no desktop")
-        for i in range(len(support_desktops)):
-            print_green(f"{i+1}. {support_desktops[i]}")
-        choose = int(input(">>> "))
-        if choose < 0 or choose > len(support_desktops):
-            print_red("Invalid input please choose again")
-            choose = int(input(">>> "))
-
-        if choose == 0:
-            self.desktop = None
+    def _detect_boot(self):
+        """自动检测启动类型"""
+        if os.path.exists("/sys/firmware/efi/efivars"):
+            self.boot = UEFI
         else:
-            self.desktop = support_desktops[choose-1]
+            self.boot = BIOS
+        
+    def _detect_cpu_vendor(self):
+        """自动检测CPU类型"""
+        self.cpu_vendor = run_cmd("lscpu | grep Vendor | awk '{print $3}'", False, False)
 
-    @just_run("set_root_password")
-    def set_root_password(self):
-        print_purple("Please set root password")
-        passwd = input(">>> ")
-        if len(passwd) == 0:
-            print_red("Password is empty, please input again")
-            passwd = input(">>> ")
-        self.root_passwd = passwd
-
-    @just_run("set_common_users")
-    def set_common_users(self):
-        if self.desktop is None:
-            print_purple("Do you need a common user? [Y/n]")
-            ys = input(">>> ").lower()
-            if ys == "n":
-                return
-
-        i = 1
-        username_list = []
-        while True:
-            print_purple(f"Please input the {i} user's name")
-            username = input(">>> ")
-            while username == "root" or username in username_list:
-                if username == "root":
-                    print_red("Username can't be `root`, please input again")
-                else:
-                    print_red(f"Username `{username}` was used, please input again")
-                username = input(">>> ")
-            print_purple(f"Please input the {i} user's password")
-            password = input(">>> ")
-            while len(password) == 0:
-                print_red("Password is empty, please input again")
-                password = input(">>> ")
-            print_purple(f"Please input the {i} user's shell [bash/zsh/fish]")
-            shell = input(">>> ")
-            while len(shell) == 0 or shell not in support_shells:
-                if len(shell) == 0:
-                    print_red("Shell is empty, please input again")
-                else:
-                    print_red(f"Shell `{shell}` is not supported, please input again")
-                shell = input(">>> ")
-
-            self.common_users.append({
-                "username": username,
-                "password": password,
-                "shell": shell
-            })
-            print_purple("Add another user? [Y/n]")
-            ys = input(">>> ").lower()
-            if ys == "n":
-                return
-            i += 1
-
-    @just_run("set_language")
-    def set_language(self):
-        print_purple("Please set language (en/zh)")
-        language = input(">>> ").lower()
-        if len(language) == 0 or language not in support_language:
-            if len(language) == 0:
-                print_red("Language is empty, please input again")
+    def set_disk_mount(self):
+        all_disk = run_cmd("fdisk -l | grep 'Disk /dev/' | awk '{print $2}'", False, True).split("\n")
+        real_disks = []
+        for d in all_disk:
+            if d.count("loop") > 0 or d.count("rom") > 0 or d.count("airoot") > 0 or d.count("ram") > 0:
+                pass
             else:
-                print_red(f"Language `{language}` is not supported, please input again")
-            language = input(">>> ")
-        self.language = language
+                real_disks.append(d[:len(d)-1])
+        
+        if len(real_disks) == 0:
+            print("%s" % apply_red("there no disk in this node!"))
+            sys.exit(0)
+        elif len(real_disks) == 1:
+            self.install_disk = real_disks[0]
+        else:
+            print("%s" % apply_purple("please choose a disk from bellow"))
+            for i, d in enumerate(real_disks):
+                print("%s. %s" % (apply_yellow(i), apply_cyan(d)))
+            while True:
+                n = input(apply_blue("please choose a number >>> "))
+                try:
+                    n = int(n)
+                except:
+                    print("%s" % apply_red("invalid input"))
+                    continue
 
-    @just_run("set_swap_size")
-    def set_swap_size(self):
-        print_purple("Please set swap size (G)")
-        swap = int(input(">>> "))
-        if swap <= 0:
-            print_red("swap must >= 1, please input again")
-            swap = int(input(">>> "))
-        self.swap_size = swap
-
-    @just_run("set_hostname")
-    def set_hostname(self):
-        print_purple("Please set hostname")
-        hostname = input(">>> ")
-        if len(hostname) == 0:
-            print_red("Hostname is empty, please input again")
-            hostname = input(">>> ")
-        self.hostname = hostname
-
-    def print_info(self):
-        print_blue("===========================")
-        for m in self.disk_mount:
-            print_blue("= ", end="")
-            print_green("disk mount: ", end="")
-            print_cyan(f"{m.disk} => {m.mount_point}")
-        print_blue("= ", end="")
-        print_green("desktop: ", end="")
-        print_cyan(f"{self.desktop}")
-        print_blue("= ", end="")
-        print_green("root passwd: ", end="")
-        print_cyan(f"{self.root_passwd}")
-        for u in self.common_users:
-            print_blue("= ", end="")
-            print_green("common user: ", end="")
-            print_cyan(f"{u['username']} | {u['password']} | {u['shell']}")
-        print_blue("= ", end="")
-        print_green("language: ", end="")
-        print_cyan(f"{self.language}")
-        print_blue("= ", end="")
-        print_green("swap size: ", end="")
-        print_cyan(f"{self.swap_size}G")
-        print_blue("= ", end="")
-        print_green("hostname: ", end="")
-        print_cyan(f"{self.hostname}")
-        print_blue("===========================")
-
-
-def run_cmd(cmd: str):
-    print_cyan("[RUN] ", end="")
-    print_yellow(cmd)
-    code = os.system(cmd)
-    if code != 0:
-        print_red("exit")
-        sys.exit(code)
-
+                if n > len(real_disks) - 1:
+                    print("%s" % apply_red("invalid input"))
+                    continue
+                else:
+                    self.install_disk = real_disks[0]
+                    break
 
 def main():
     cfg = Config()
     cfg.set_disk_mount()
-    cfg.set_desktop()
-    cfg.set_root_password()
-    cfg.set_common_users()
-    cfg.set_language()
-    cfg.set_swap_size()
-    cfg.set_hostname()
-    print_purple("All config is finish, please check the info below")
-    cfg.print_info()
-    print_yellow("Install process will clear all data in disk, are you sure going on? [N/y]")
-    ys = input(">>> ").lower()
-    if ys != "y":
-        print_yellow("Quit")
-        return
+
+    print(cfg.boot)
+    print(cfg.cpu_vendor)
+
+    # cfg.set_desktop()
+    # cfg.set_root_password()
+    # cfg.set_common_users()
+    # cfg.set_language()
+    # cfg.set_swap_size()
+    # cfg.set_hostname()
+    # print_purple("All config is finish, please check the info below")
+    # cfg.print_info()
+    # print_yellow("Install process will clear all data in disk, are you sure going on? [N/y]")
+    # ys = input(">>> ").lower()
+    # if ys != "y":
+    #     print_yellow("Quit")
+    #     return
 
     # packages
-    global base_packages
-    if cfg.boot_way == "UEFI":
-        base_packages += " efibootmgr"
+    # global base_packages
+    # if cfg.boot == "UEFI":
+    #     base_packages += " efibootmgr"
 
-    if cfg.desktop == "gnome":
-        base_packages += " networkmanager xorg alsa-utils pulseaudio pulseaudio-alsa xf86-input-synaptics ttf-dejavu wqy-microhei gdm gnome gnome-extra"
-    elif cfg.desktop == "plasma":
-        base_packages += " networkmanager xorg alsa-utils pulseaudio pulseaudio-alsa xf86-input-synaptics ttf-dejavu wqy-microhei plasma kde-applications libdbusmenu-glib appmenu-gtk-module packagekit-qt5"
+    # if cfg.desktop == "gnome":
+    #     base_packages += " networkmanager xorg alsa-utils pulseaudio pulseaudio-alsa xf86-input-synaptics ttf-dejavu wqy-microhei gdm gnome gnome-extra"
+    # elif cfg.desktop == "plasma":
+    #     base_packages += " networkmanager xorg alsa-utils pulseaudio pulseaudio-alsa xf86-input-synaptics ttf-dejavu wqy-microhei plasma kde-applications libdbusmenu-glib appmenu-gtk-module packagekit-qt5"
 
 
 if __name__ == '__main__':
